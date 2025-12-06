74b13595383b19daf98e38ea490cacaa5d9d00e5
huyufan <gaohuyufan@qq.com>
2025-12-06 11:32:56 +0800

    create nix package

---
+++ package.nix
@@ -0,0 +1,14 @@
+{ runCommand }:
+
+let
+  pname = "inx-container";
+  version = "0.0.1";
+in
+
+runCommand "${pname}-${version}"
+  { }
+  ''
+    mkdir -p $out/bin
+    install -m +x ${./inx-container} $out/bin/inx-container
+    cp ${./config.yaml} $out/bin/config.yaml
+  ''
